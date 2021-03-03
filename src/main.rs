0b029056fa720ea22ea766c9660d080a3239102c
Dennis Ranke <dennis.ranke@gmail.com>
2021-03-03 13:02:09 +0100

    add more robust whitespace stripping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod tic_file;
+mod whitespace;
 
 use anyhow::{anyhow, bail, Result};
 use clap::Clap;
@@ -29,7 +30,7 @@ fn main() -> Result<()> {
     match opts.cmd {
         SubCommand::Pack(pack) => pack.exec()?,
         SubCommand::Extract(cmd) => cmd.exec()?,
-        SubCommand::Empty(cmd) => cmd.exec()?
+        SubCommand::Empty(cmd) => cmd.exec()?,
     }
 
     Ok(())
@@ -100,7 +101,7 @@ impl CmdPack {
 
         let mut code = code.ok_or_else(|| anyhow!("No code chunk found"))?;
         if !self.no_strip_whitespace {
-            code = strip_whitespace(&code);
+            code = whitespace::strip_whitespace(&code);
         }
 
         if self.new_palette {
@@ -177,7 +178,6 @@ impl CmdEmpty {
 }
 
 fn compress_code(code: Vec<u8>) -> tic_file::Chunk {
-    let code = strip_whitespace(&code);
     println!("Uncompressed size: {:5} bytes", code.len());
     let mut data = vec![];
     zopfli::compress(
@@ -189,167 +189,17 @@ fn compress_code(code: Vec<u8>) -> tic_file::Chunk {
     .unwrap();
     data.truncate(data.len() - 4);
     println!("  Compressed size: {:5} bytes", data.len());
-    tic_file::Chunk {
-        type_: 0x10,
-        bank: 0,
-        data,
-    }
-}
-
-fn strip_whitespace(code: &[u8]) -> Vec<u8> {
-    let mut stripped = vec![];
-
-    #[derive(Eq, PartialEq)]
-    enum State {
-        Normal,
-        Minus,
-        Comment,
-        Identifier,
-        WsAfterIdentifier,
-        Number,
-        WsAfterNumber,
-        String(u8),
-        StringEscape(u8),
-    }
-
-    let mut state = State::Normal;
-    for &c in code {
-        match state {
-            State::Normal => match c {
-                b' ' | b'\n' | b'\r' | b'\t' => (),
-                b'_' | b'A'..=b'Z' | b'a'..=b'z' => {
-                    state = State::Identifier;
-                    stripped.push(c);
-                }
-                b'0'..=b'9' | b'.' => {
-                    state = State::Number;
-                    stripped.push(c);
-                }
-                b'-' => state = State::Minus,
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                _ => stripped.push(c),
-            },
-            State::Minus => match c {
-                b'-' => state = State::Comment,
-                b'_' | b'A'..=b'Z' | b'a'..=b'z' => {
-                    state = State::Identifier;
-                    stripped.push(b'-');
-                    stripped.push(c);
-                }
-                b'0'..=b'9' | b'.' => {
-                    state = State::Number;
-                    stripped.push(b'-');
-                    stripped.push(c);
-                }
-                b' ' | b'\n' | b'\r' | b'\t' => {
-                    state = State::Normal;
-                    stripped.push(b'-');
-                }
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                _ => {
-                    state = State::Normal;
-                    stripped.push(b'-');
-                    stripped.push(c);
-                }
-            },
-            State::Comment => match c {
-                b'\n' | b'\r' => state = State::Normal,
-                _ => (),
-            },
-            State::Identifier => match c {
-                b' ' | b'\n' | b'\r' | b'\t' => state = State::WsAfterIdentifier,
-                b'_' | b'0'..=b'9' | b'A'..=b'Z' | b'a'..=b'z' => stripped.push(c),
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                b'-' => state = State::Minus,
-                _ => {
-                    state = State::Normal;
-                    stripped.push(c);
-                }
-            },
-            State::Number => match c {
-                b' ' | b'\n' | b'\r' | b'\t' => state = State::WsAfterNumber,
-                b'0'..=b'9' | b'A'..=b'F' | b'a'..=b'f' | b'X' | b'x' | b'.' => stripped.push(c),
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                b'-' => state = State::Minus,
-                _ => {
-                    state = State::Normal;
-                    stripped.push(c);
-                }
-            },
-            State::WsAfterIdentifier => match c {
-                b' ' | b'\n' | b'\r' | b'\t' => (),
-                b'_' | b'A'..=b'Z' | b'a'..=b'z' => {
-                    state = State::Identifier;
-                    stripped.push(b' ');
-                    stripped.push(c);
-                }
-                b'0'..=b'9' | b'.' => {
-                    state = State::Number;
-                    stripped.push(b' ');
-                    stripped.push(c);
-                }
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                b'-' => state = State::Minus,
-                _ => {
-                    state = State::Normal;
-                    stripped.push(c)
-                }
-            },
-            State::WsAfterNumber => match c {
-                b' ' | b'\n' | b'\r' | b'\t' => (),
-                b'A'..=b'F' | b'a'..=b'f' => {
-                    state = State::Identifier;
-                    stripped.push(b' ');
-                    stripped.push(c);
-                }
-                b'_' | b'G'..=b'Z' | b'g'..=b'z' => {
-                    state = State::Identifier;
-                    stripped.push(c);
-                }
-                b'0'..=b'9' | b'.' => {
-                    state = State::Number;
-                    stripped.push(b' ');
-                    stripped.push(c);
-                }
-                b'\'' | b'"' => {
-                    state = State::String(c);
-                    stripped.push(c);
-                }
-                b'-' => state = State::Minus,
-                _ => {
-                    state = State::Normal;
-                    stripped.push(c)
-                }
-            },
-            State::String(q) => {
-                stripped.push(c);
-                if c == q {
-                    state = State::Normal;
-                } else if c == b'\\' {
-                    state = State::StringEscape(q);
-                }
-            }
-            State::StringEscape(q) => {
-                stripped.push(c);
-                state = State::String(q);
-            }
+    if code.len() <= data.len() {
+        tic_file::Chunk {
+            type_: 0x05,
+            bank: 0,
+            data: code,
+        }
+    } else {
+        tic_file::Chunk {
+            type_: 0x10,
+            bank: 0,
+            data,
         }
     }
-
-    stripped
 }
